a028553eed87975505701cdbd6705878bde23f24
elyyaaa <155648362+elyyaaa@users.noreply.github.com>
2024-04-06 20:06:24 +0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import './App.css';
-import PokemonPageClass from "./components /page/pokemonPage/PokemonPageClass";
-import PokemonPage from "./components /page/pokemonPage/PokemonPage";
-import CountPageClass from "./components /page/countPageClass/CountPageClass";
-import CountPage from "./components /page/countPage/CountPage";
-import UserInfo from "./components /page/userInfo/UserInfo";
-import Users from "./components /page/users/Users";
-import FormPage from "./components /page/formPage/FormPage";
-import Menu from "./components /menu/Menu";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import PokemonInfoPage from "./components /page/pokemonInfoPage/PokemonInfoPage";
+import UserRegister from "./pages/userRegister/UserRegister";
+
 function App() {
-     return(
-         <BrowserRouter>
-             <Menu/>
-             <Routes>
-                 <Route path='/' element={<PokemonPage/>}/>
-                 <Route path='/pokemon-class' element={<PokemonPageClass/>}/>
-                 <Route path='/count' element={<CountPage/>}/>
-                 <Route path='/countClass' element={<CountPageClass/>}/>
-                 <Route path='/users' element={<Users/>}/>
-                 <Route path='/form' element={<FormPage/>}/>
-                 <Route path='/users/:id' element={<UserInfo/>}/>
-                 <Route path='/pokemon/:id' element={<PokemonInfoPage/>}/>
-             </Routes>
-
-         </BrowserRouter>
-     )
+    return (
+        <div>
+            <UserRegister/>
+        </div>
+    );
 }
-
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {applyMiddleware, createStore} from 'redux'
+import { Provider } from 'react-redux';
+import {thunk} from "redux-thunk";
+import {logger} from "redux-logger/src";
+
+const store = createStore( applyMiddleware(logger,thunk))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
+
